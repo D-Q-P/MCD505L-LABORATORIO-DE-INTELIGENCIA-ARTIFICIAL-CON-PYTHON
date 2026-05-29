320b01bf0a5605f9743fb13bdb2d32bbe7d151c9
D-Q-P <david.quezada.p@gmail.com>
2026-05-29 10:59:53 -0400

    Update Problema 1.py

--- Practica 2/Problema 1.py
+++ Practica 2/Problema 1.py
@@ -5,5 +5,5 @@ def calcular_padovan(n):
         padovan_list.append(1) if i <= 3 else padovan_list.append(padovan_list[i-3] + padovan_list[i-4])
     print(f"El valor de la sucesion padovan para el elemento {sucesion} es: {padovan_list[sucesion-1]}")
 
-sucesion = int(input("Numero de sucecion: "))
+sucesion = int(input("Número de sucesion: "))
 calcular_padovan(sucesion)
