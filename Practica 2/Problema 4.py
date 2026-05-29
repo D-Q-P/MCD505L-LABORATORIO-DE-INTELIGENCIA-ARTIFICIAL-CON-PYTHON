64191f394714c3f32752823c2dc0856f92377756
D-Q-P <david.quezada.p@gmail.com>
2026-05-29 11:02:59 -0400

    Update Problema 4.py

--- Practica 2/Problema 4.py
+++ Practica 2/Problema 4.py
@@ -2,7 +2,6 @@ import random as rand
 
 numero_magico = rand.randint(1,10)
 intentos = 0
-print(f"El numero magico es: {numero_magico}")
 while True:
     intentos += 1
     numero = int(input("Adivina el número: "))
